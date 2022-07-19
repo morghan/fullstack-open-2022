ada7496c62b64369d0f297ac29c5b361eed0ee11
jerzonhr <106408622+jerzonhr@users.noreply.github.com>
2022-07-18 18:00:52 -0600

    exercise1.12-1.14 - 2h

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+const App = () => {
+	const anecdotes = [
+		'If it hurts, do it more often.',
+		'Adding manpower to a late software project makes it later!',
+		'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+		'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+		'Premature optimization is the root of all evil.',
+		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+		'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+	];
+
+	const [selected, setSelected] = useState(0);
+	const [anecdotesData, setAnecdotesData] = useState({
+		anecdotesVotes: new Array(anecdotes.length).fill(0),
+		mostVotedAnecdote: 0,
+	});
+
+	const selectRandom = () => {
+		const random = Math.floor(Math.random() * anecdotes.length);
+		setSelected(random);
+	};
+
+	const voteAnecdote = () => {
+		let anecdotesDataCopy = { ...anecdotesData };
+		const mostVotedAnecdote = anecdotesDataCopy.mostVotedAnecdote;
+		const mostVotes = anecdotesDataCopy.anecdotesVotes[mostVotedAnecdote];
+
+		anecdotesDataCopy.anecdotesVotes[selected]++;
+
+		anecdotesDataCopy.anecdotesVotes.forEach((vote, index) => {
+			if (vote > mostVotes) anecdotesDataCopy.mostVotedAnecdote = index;
+		});
+		setAnecdotesData(anecdotesDataCopy);
+	};
+
+	return (
+		<div>
+			<div>
+				<h2>Anecdote of the day</h2>
+				<p>{anecdotes[selected]}</p>
+				<p>Has {anecdotesData.anecdotesVotes[selected]} votes </p>
+				<button onClick={voteAnecdote}>Vote</button>
+				<button onClick={selectRandom}>Another anecdote</button>
+			</div>
+			<div>
+				<h3>Anecdote with most votes</h3>
+				<p>{anecdotes[anecdotesData.mostVotedAnecdote]}</p>
+				<p>
+					Has {anecdotesData.anecdotesVotes[anecdotesData.mostVotedAnecdote]}{' '}
+					votes{' '}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default App;
