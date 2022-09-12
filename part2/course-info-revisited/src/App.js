d8c5595bd8b59b925ba38401674a29c59d7cc0bc
jerzonhr <106408622+jerzonhr@users.noreply.github.com>
2022-09-12 17:10:09 -0600

    📦 NEW: Add Course component

---
+++ part2/course-info-revisited/src/App.js
@@ -0,0 +1,54 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+const Part = (props) => (
+	<p>
+		{props.partName} {props.partExercises}
+	</p>
+);
+const Content = ({ parts }) => (
+	<div>
+		{parts.map((part) => (
+			<Part partName={part.name} partExercises={part.exercises} key={part.id} />
+		))}
+	</div>
+);
+// const Total = (props) => {
+// 	let total = 0;
+// 	props.parts.forEach((part) => (total += part.exercises));
+// 	return <p>Number of exercises {total}</p>;
+// };
+
+const Course = ({ course }) => (
+	<div>
+		<Header courseName={course.name} />
+		<Content parts={course.parts} />
+	</div>
+);
+
+const App = () => {
+	const course = {
+		id: 1,
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+				id: 3,
+			},
+		],
+	};
+
+	return <Course course={course} />;
+};
+
+export default App;
