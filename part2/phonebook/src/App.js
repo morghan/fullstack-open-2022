ed1a7ab9887ef2096fd216234f8934ead9c415d0
morghan <joel.hz24@gmail.com>
2022-10-04 10:05:00 -0600

    📦 NEW: exercise2.8 ~ 0.25h

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,42 +2,65 @@ import { useState } from 'react'
 
 const Person = ({ person }) => (
 	<span>
-		{person.name}
+		{person.name} : {person.number}
 		<br />
 	</span>
 )
 const App = () => {
-	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	])
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
 	}
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
+	}
+
 	const addPerson = (event) => {
 		event.preventDefault()
-		const personFound = persons.find(
+		const nameFound = persons.find(
 			(person) => person.name.toLowerCase() === newName.toLowerCase()
 		)
-		if (personFound) alert(`${newName} is already added to the phonebook`)
-		if (newName.trim() && !personFound) {
+		if (nameFound) {
+			alert(`${newName} is already added to the phonebook`)
+			return
+		}
+		if (newName.trim() && newNumber.trim()) {
 			const newPerson = {
 				name: newName,
+				number: newNumber,
+				id: persons.length + 1,
 			}
 			setPersons(persons.concat(newPerson))
 			setNewName('')
-		}
+			setNewNumber('')
+		} else alert('Name or number is empty')
 	}
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<div>debug: {newName}</div>
 			<form onSubmit={addPerson}>
 				<div>
 					name:{' '}
 					<input
 						value={newName}
 						onChange={handleNameChange}
-						placeholder='add Person name'
+						placeholder='add name'
+					/>
+				</div>
+				<div>
+					number:{' '}
+					<input
+						value={newNumber}
+						onChange={handleNumberChange}
+						placeholder='add number'
 					/>
 				</div>
 				<div>
@@ -46,7 +69,7 @@ const App = () => {
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person) => (
-				<Person key={person.name} person={person} />
+				<Person key={person.id} person={person} />
 			))}
 		</div>
 	)
