7c247d7c37a036c9b894bb4afd48bc9ceef2ae67
morghan <joel.hz24@gmail.com>
2022-10-06 14:54:27 -0600

    🐛 FIX: Add useEffect to handle countries' visibility state

--- part2/country-search-app/src/components/CountryList.js
+++ part2/country-search-app/src/components/CountryList.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Country } from './Country'
 
 export const CountryList = ({ countries }) => {
@@ -6,12 +6,18 @@ export const CountryList = ({ countries }) => {
 	// the list of countries is empty
 	const [visible, setVisible] = useState([])
 
-	// This handler actually populates the visible state per click
 	const toggleVisible = (index) => {
 		const copy = { ...visible }
 		copy[index] = !copy[index]
 		setVisible(copy)
 	}
+	// useEffect hook populates state when list of countries changes
+	useEffect(() => {
+		if (countries.length === 1) setVisible([true])
+		if (countries.length > 0 && countries.length <= 10) {
+			setVisible(new Array(countries.length).fill(false))
+		}
+	}, [countries])
 
 	if (countries.length === 1) {
 		return <Country country={countries[0]} />
